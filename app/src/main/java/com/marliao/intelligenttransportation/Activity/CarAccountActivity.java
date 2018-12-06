fe88b2ba46e25decaccea6a1384d7ddcb1eac9db
marliao <892564305@qq.com>
2018-12-06 22:00:32 +0800

    充值与查询

---
+++ app/src/main/java/com/marliao/intelligenttransportation/Activity/CarAccountActivity.java
@@ -0,0 +1,15 @@
+package com.marliao.intelligenttransportation.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.marliao.intelligenttransportation.R;
+
+public class CarAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_account);
+    }
+}
